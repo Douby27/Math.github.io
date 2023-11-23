e5e249a192251d41272ed0caaaa55039dd9dce5c
Douby27 <148044699+Douby27@users.noreply.github.com>
2023-11-23 22:20:01 +0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,55 @@
+//variabel 
+let A = 4.5;
+let B = 6.3;
+let C = 6.6;
+
+//math round
+let round1 = Math.round(A);
+console.log(round1);
+let round2 = Math.round(B);
+console.log(round2);
+let round3 = Math.round(C);
+console.log(round3);
+
+//math ceil
+let ceil1 = Math.ceil(A);
+console.log(ceil1);
+let ceil2 = Math.ceil(B);
+console.log(ceil2);
+let ceil3 = Math.ceil(C);
+console.log(ceil3);
+
+//math floor
+let floor1 = Math.floor(A);
+console.log(floor1);
+let floor2 = Math.floor(B);
+console.log(floor2);
+let floor3 = Math.floor(C);
+console.log(floor3);
+
+//math pow
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+
+let pow1 = Math.pow(a,d);
+console.log(pow1);
+let pow2 = Math.pow(b,f);
+console.log(pow2);
+let pow3 = Math.pow(c,e);
+console.log(pow3);
+
+//math sqrt
+let g = 25
+let h = 225
+let i = 625
+
+let sqrt1 = Math.sqrt(g);
+console.log(sqrt1);
+let sqrt2 = Math.sqrt(h);
+console.log(sqrt2);
+let sqrt3 = Math.sqrt(i);
+console.log(sqrt3);
